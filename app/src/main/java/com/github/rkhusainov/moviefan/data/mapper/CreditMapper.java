66db372ab66ba92ed18138c35ec817f3f03fea40
Rinat Khusainov <rinat.khusainov.msk@gmail.com>
2019-12-10 15:29:08 +0300

    Fix credit mapper

--- app/src/main/java/com/github/rkhusainov/moviefan/data/mapper/CreditMapper.java
+++ app/src/main/java/com/github/rkhusainov/moviefan/data/mapper/CreditMapper.java
@@ -2,7 +2,9 @@ package com.github.rkhusainov.moviefan.data.mapper;
 
 import androidx.annotation.NonNull;
 
+import com.github.rkhusainov.moviefan.data.model.credit.Cast;
 import com.github.rkhusainov.moviefan.data.model.credit.Credit;
+import com.github.rkhusainov.moviefan.data.model.credit.Crew;
 import com.github.rkhusainov.moviefan.domain.mapper.IMapper;
 import com.github.rkhusainov.moviefan.domain.model.CastEntity;
 import com.github.rkhusainov.moviefan.domain.model.CreditEntity;
@@ -16,15 +18,34 @@ public class CreditMapper implements IMapper<Credit, CreditEntity> {
     @Override
     public CreditEntity convert(@NonNull Credit credit) {
 
-        List<CastEntity> casts = new ArrayList(credit.getCast());
-        List<CrewEntity> crews = new ArrayList(credit.getCrew());
+        List<CastEntity> casts = new ArrayList<>();
 
-//        for (int i = 0; i < credit.getCast().size(); i++) {
-//            casts.add(new CastEntity(
-//                    credit.getCast().get(i).getId(),
-//                    credit.getCast().get(i).
-//            ));
-//        }
+        for (Cast cast : credit.getCast()) {
+            casts.add(new CastEntity(
+                    cast.getId(),
+                    cast.getCastId(),
+                    cast.getCharacter(),
+                    cast.getCreditId(),
+                    cast.getGender(),
+                    cast.getName(),
+                    cast.getOrder(),
+                    cast.getProfilePath()
+            ));
+        }
+
+        List<CrewEntity> crews = new ArrayList<>();
+
+        for (Crew crew : credit.getCrew()) {
+            crews.add(new CrewEntity(
+                    crew.getId(),
+                    crew.getCreditId(),
+                    crew.getDepartment(),
+                    crew.getGender(),
+                    crew.getJob(),
+                    crew.getName(),
+                    crew.getProfilePath()
+            ));
+        }
 
         return new CreditEntity(
                 credit.getId(),
--- app/src/main/java/com/github/rkhusainov/moviefan/data/mapper/CrewMapper.java
+++ app/src/main/java/com/github/rkhusainov/moviefan/data/mapper/CrewMapper.java
@@ -6,18 +6,26 @@ import com.github.rkhusainov.moviefan.data.model.credit.Crew;
 import com.github.rkhusainov.moviefan.domain.mapper.IMapper;
 import com.github.rkhusainov.moviefan.domain.model.CrewEntity;
 
-public class CrewMapper implements IMapper<Crew, CrewEntity> {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CrewMapper implements IMapper<List<Crew>, List<CrewEntity>> {
     @NonNull
     @Override
-    public CrewEntity convert(@NonNull Crew crew) {
-        return new CrewEntity(
-                crew.getId(),
-                crew.getCreditId(),
-                crew.getDepartment(),
-                crew.getGender(),
-                crew.getJob(),
-                crew.getName(),
-                crew.getProfilePath()
-        );
+    public List<CrewEntity> convert(@NonNull List<Crew> crews) {
+        List<CrewEntity> entityList = new ArrayList<>();
+
+        for (Crew crew : crews) {
+            entityList.add(new CrewEntity(
+                    crew.getId(),
+                    crew.getCreditId(),
+                    crew.getDepartment(),
+                    crew.getGender(),
+                    crew.getJob(),
+                    crew.getName(),
+                    crew.getProfilePath()
+            ));
+        }
+        return entityList;
     }
 }
